dc5c0f465c52ead3ec00c0f962dbd62c68f11c60
samrawit <samrawitlwasihun@gmail.com>
2022-12-13 12:18:37 -0800

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char ch= 'a';
+
+	while(ch <= 'z')
+	{
+		_putchar(ch);
+	
+	ch++;
+	_putchar('\n');
+	}
+}
