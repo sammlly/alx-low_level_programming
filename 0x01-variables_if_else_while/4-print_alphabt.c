76c05a7b0ea50e43f76ae343e294cbcd4cbfa408
samrawit <samrawitlwasihun@gmail.com>
2022-12-12 18:08:14 -0800

    all tasks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main returns selected alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch= 'a';
+	while (ch <= 'z')
+	{		
+	if((ch!= 'e') && (ch!= 'q'))
+		{
+			putchar(ch);
+		}
+	ch++;
+	}
+	putchar('\n');
+	return (0);
+}
