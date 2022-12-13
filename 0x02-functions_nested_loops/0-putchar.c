37057074c090b7f9798d9e06e92a111e60d5d755
samrawit <samrawitlwasihun@gmail.com>
2022-12-13 10:37:22 -0800

    c1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,17 +6,17 @@
  * Return: Always return 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-	_putchar('_');
-        _putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 	return (0);
 }
