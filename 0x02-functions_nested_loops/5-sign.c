afd294675f3ded97f41c1f8741a336a86782fe42
samrawit <samrawitlwasihun@gmail.com>
2022-12-14 12:09:57 -0800

    test 2

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point.
+ * @n: represents a number.
+ * Return: returns 1, 0, -1 for different n values.
+ */
+
+int print_sign(int n)
+{
+	int test;
+
+	if (n > 0)
+	{
+		test = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	}
+	else
+	{
+		test = -1;
+		_putchar('-');
+	}
+	return (test);
+}	
